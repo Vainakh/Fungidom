d500c9e48d25c37b99261dc6acb2915ce66d5080
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-10 20:35:08 -0800

    Add styling to Items component and to main component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Footer.css';
+import Aux from '../../hoc/Aux/Aux';
 
 const footer = () => (
-  <div className="Footer">
-    Footer
-  </div>
+  <Aux>
+    <div className="PreFooter">Check out our latest products and offerings!</div>
+    <div className="Footer">
+      <div className="FooterInfo">
+        <div>Stay in touch. Sign up for our newletter!</div>
+        <div>
+          <ul> 
+            <li>About</li>
+            <li>Our Products</li>
+            <li>FAQ</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </Aux>
+  
 )
 
 export default footer; 
