7c089136e16c98426700b033b0b006893e5b426b
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2021-01-11 10:36:55 -0800

    Create a basic backend set up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Footer.css';
 import Aux from '../../hoc/Aux/Aux';
+import mushroom from '../../assets/images/mushroom.svg';
+
 
 const footer = () => (
   <Aux>
     <div className="PreFooter">Check out our latest products and offerings!</div>
     <div className="Footer">
-      <div className="FooterInfo">
-        <div>Stay in touch. Sign up for our newletter!</div>
-        <div>
+      <div>
+        <div className="FooterInfo">Stay in touch. Sign up for our newletter!</div>
+        <div className="FooterUl">
           <ul> 
             <li>About</li>
             <li>Our Products</li>
@@ -16,10 +18,10 @@ const footer = () => (
             <li>Contact</li>
           </ul>
         </div>
+        <img className="FooterImage" src={mushroom} alt="MushroomLogo"></img>
       </div>
-    </div>
-  </Aux>
-  
-)
+     </div>
+  </Aux> 
+);
 
 export default footer; 
